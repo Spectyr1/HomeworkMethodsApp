b6001d2159eff2f2aea072834b32dff39f3947ae
Danny Warren <danny@theforbiddenarts.com>
2022-04-02 16:46:09 -0400

    Add project files.

---
+++ HomeworkMethods/Greetings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkMethods
+{
+    public static class Greetings
+    {
+        public static void SayHi()
+        {
+            Console.WriteLine("Hello user, welcome to my application.");
+        }
+
+        public static string GetName()
+        {
+            Console.Write("Please enter your name: ");
+            return Console.ReadLine();
+        }
+
+        public static void Greeting(string name)
+        {
+            Console.WriteLine($"Nice to meet you {name}. Hope you have a wonderful day");
+        }
+    }
+}
---
+++ HomeworkMethods/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+using HomeworkMethods;
+
+
+Greetings.SayHi();
+string name = Greetings.GetName();
+Greetings.Greeting(name);
